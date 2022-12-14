211d161d27c3cb72f7252b08036e95e5b3426197
maripiero23 <mari.pieropan@gmail.com>
2022-12-14 16:42:38 +0100

    modifiche

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -3,17 +3,17 @@
     <div class="container">
 
       <div class="row">
-        <div class="col-2 gap-4" v-for="(film, index) in movieDetails" key="index">
+        <div class="col-2 gap-4" v-for="(series, index) in movieDetails" key="index">
           <div class="card-body">
     
             <div class="overflow">
-              <img v-if="film.backdrop_path == null" src="https://picsum.photos/185/105" alt="">
-              <img v-else :src="'https://image.tmdb.org/t/p/w342'+ film.backdrop_path"  alt="" class="img-fluid">
+              <img v-if="series.backdrop_path == null" src="https://picsum.photos/185/282" alt="">
+              <img v-else :src="'https://image.tmdb.org/t/p/w342'+ series.poster_path"  alt="" class="img-fluid">
             </div>
-            <h5 class="card-title">Titolo:{{film.title}}</h5>
-            <p class="card-text">Titolo originale:{{film.original_title}}</p>
-            <p class="card-text">Lingua originale:{{film.original_language}}</p>
-            <p class="card-text">Voto:{{film.vote_average}}</p>
+            <h5 class="card-title">Titolo:{{series.title}}</h5>
+            <p class="card-text">Titolo originale:{{series.original_title}}</p>
+            <p class="card-text">Lingua originale:{{series.original_language}}</p>
+            <p class="card-text">Voto:{{series.vote_average}}</p>
             <p class="card-text"></p>
           </div>
         </div>
